75065b50ef03ce4acffea0496321725c06b23540
Aleksa.Sibinovic <aleksa.sibinovic@adacta-fintech.com>
2025-03-13 13:11:06 +0100

    days 4 and 5 completed

--- calendar_2024/day04_Ceres_Search.py
+++ calendar_2024/day04_Ceres_Search.py
@@ -1,25 +1,27 @@
-import numpy as np
-
 with open('calendar_2024/data/day4.txt') as file:
-    M = np.array([[ch for ch in line.strip()] for line in file])
-print(M)
-num = 0
-check_obj = ['X','M','A','S']
-check_rev = check_obj.reverse()
-for i in range(M.shape[0]):
-    for j in range(M.shape[1]): # zašto nije htelo M[0].size?
-        # check for x
-        if M[i,j] == 'X':
-            if M[i,j-3:j+1] == check_rev: # <-
-                num += 1
-                continue
-            if M[i,j:j+4] == check_obj: # ->
-                num += 1
-                continue
-            if M[i-3:i+1,j] == check_rev: # ^
-                num += 1
-                continue
-            if M[i:i+4,j] == check_obj: # v
-                num += 1
-                continue
-            ... # dijagonale numpy
+    M = file.read().splitlines()
+
+# Part 1
+
+count = 0
+for r in range(len(M)):
+    for c in range(len(M[0])):
+        if M[r][c] != "X": continue
+        for dr in [-1, 0, 1]: # directions
+            for dc in [-1, 0, 1]:
+                if dr == dc == 0: continue
+                if not (0 <= r + 3 * dr < len(M) and 0 <= c + 3 * dc < len(M[0])): continue
+                if M[r + dr][c + dc] == "M" and M[r + 2 * dr][c + 2 * dc] == "A" and M[r + 3 * dr][c + 3 * dc] == "S":
+                    count += 1
+print(count)
+
+# Part 2
+
+count = 0
+for r in range(1, len(M)-1):
+    for c in range(1, len(M[0])-1):
+        if M[r][c] != "A": continue
+        corners = [M[r-1][c-1], M[r-1][c+1], M[r+1][c+1], M[r+1][c-1]] # clockwise order of corners
+        if "".join(corners) in ["MMSS", "MSSM", "SSMM", "SMMS"]:
+            count += 1
+print(count)
--- calendar_2024/day05_Print_Queue.py
+++ calendar_2024/day05_Print_Queue.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 page_rules = defaultdict(list)
-no_of_correct = 0
+sum = 0
 with open('calendar_2024/data/day5.txt') as file:
     for line in file:
         if line == "\n":
@@ -9,14 +9,38 @@ with open('calendar_2024/data/day5.txt') as file:
         page_rules[l].append(r)
     for line in file:
         valid = True
-        arr = line.split(",")
+        arr = [int(x) for x in line.split(",")]
         for i in range(len(arr)-1):
             for j in range(i+1, len(arr)):
-                if j in page_rules.keys() and i in page_rules[j]:
+                if arr[j] in page_rules.keys() and arr[i] in page_rules[arr[j]]:
                     valid = False
                     break
             if not valid:
                 break
         if valid:
-            no_of_correct += 1
-print(no_of_correct)
+
+            sum += int(arr[len(arr)//2])
+print(sum)
+
+# Part 2
+
+sum = 0
+with open('calendar_2024/data/day5.txt') as file:
+    for line in file:
+        if line == "\n":
+            break
+        l, r = map(int,line.split("|"))
+        page_rules[l].append(r)
+    for line in file:
+        valid = True
+        arr = [int(x) for x in line.split(",")]
+        for i in range(len(arr)-1):
+            for j in range(i+1, len(arr)):
+                if arr[j] in page_rules.keys() and arr[i] in page_rules[arr[j]]:
+                    valid = False
+                    arr[i], arr[j] = arr[j], arr[i]
+        if not valid:
+
+            sum += int(arr[len(arr)//2])
+# < 5784
+print(sum)
